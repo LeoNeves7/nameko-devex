70a6b76c159d78e1a253111cab0461ada8e3acf7
leonardoneves <leonardo.neves@ilia.digital>
2023-09-13 13:53:47 -0300

    :lipstick: create routing transition

--- gateapi/web/src/routes/+layout.svelte
+++ gateapi/web/src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-	import '../app.css';
+	import '../app.css'
+	import { fly } from 'svelte/transition'
+
+	export let data
 </script>
 
-<main>
-	<slot />
-</main>
+{#key data.url}
+	<div
+		in:fly={{ x: -200, duration: 300, delay: 300 }}
+		out:fly={{ x: 200, duration: 300 }}
+	>
+		<slot />
+	</div>
+{/key}
