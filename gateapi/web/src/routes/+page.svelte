5e97cef3e5b4bd60fc31ad0444be5fc75b5266be
leonardoneves <leonardo.neves@ilia.digital>
2023-09-13 13:53:02 -0300

    :lipstick: update nav styles

--- gateapi/web/src/routes/+page.svelte
+++ gateapi/web/src/routes/+page.svelte
@@ -1,13 +1,32 @@
-<script context="module" lang="ts">
+<script lang="ts">
 	import { ROUTES } from './routes'
+	import { page } from '$app/stores'
+
+	let path: string
+
+	$: path = $page.url.pathname
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<main>
+<main
+	class="flex py-4 h-16 bg-white justify-center items-center gap-6 shadow-primary-shadow"
+>
 	{#each Object.entries(ROUTES) as [name, route]}
-		<a class="mx-2 text-blue-400 underline" href={route}>{name}</a>
+		<a
+			class="text-s text-primary transition-transform duration-300 hover:text-accent hover:scale-105 hover:translate-y-[-1px]"
+			class:current-page={path === route}
+			href={route}
+		>
+			{name}
+		</a>
 	{/each}
 </main>
+
+<style lang="scss">
+	.current-page {
+		@apply text-accent underline underline-offset-4;
+	}
+</style>
