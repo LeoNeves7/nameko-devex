6ee163c536c306fbee66b37b7c30d96cbac00cb0
leonardoneves <leonardo.neves@ilia.digital>
2023-09-13 13:51:51 -0300

    :lipstick: setup custom tailwind variables

--- gateapi/web/tailwind.config.cjs
+++ gateapi/web/tailwind.config.cjs
@@ -1,7 +1,34 @@
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.svelte'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.html', './src/**/*.svelte'],
+	theme: {
+		extend: {},
+		colors: {
+			primary: '#35344C',
+			secondary: '#C3C1D7',
+			'background-primary': '#FCFCFD',
+			accent: '#745CF1',
+			purple: '#3F25C8',
+			'light-purple': '#F3F1FE',
+			grey: '#474374',
+			white: '#FFFFFF',
+			'white-secondary': '#E5E5E4',
+		},
+		fontFamily: {
+			inter: ['Inter'],
+		},
+		fontSize: {
+			xxl: '2.188rem',
+			m: '1.125rem',
+			s: '1rem',
+			xs: '0.938rem',
+			xxs: '0.875rem',
+		},
+		dropShadow: {
+			primary: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+		},
+		boxShadow: {
+			'primary-shadow': '0px 4px 16px rgba(0, 0, 0, 0.1)',
+		},
+	},
+	plugins: [],
 }
