a1bacd1cb16ab1359e4754a0f67685ed8db760a1
leonardoneves <leonardo.neves@ilia.digital>
2023-09-13 15:50:07 -0300

    :white-check-mark: add UI tests for the order page

---
+++ gateapi/web/tests/page-two.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@playwright/test'
+
+test('page two components must be defined', async ({ page }) => {
+	await page.goto('/page-two')
+
+	expect(page.getByTestId('order-details-title')).toBeDefined()
+
+	expect(page.getByTestId('orders-wrapper')).toBeDefined()
+
+	expect(page.getByText('BACK')).toBeDefined()
+})
+
+test('back button should redirect to home page', async ({ page }) => {
+	await page.goto('/page-two')
+
+	const backButton = page.getByText('BACK')
+	expect(backButton).toBeDefined()
+
+	await backButton.click()
+
+	await page.waitForURL('/')
+
+	const currentURL = page.url()
+	expect(currentURL).toBe('http://localhost:3000/')
+})
