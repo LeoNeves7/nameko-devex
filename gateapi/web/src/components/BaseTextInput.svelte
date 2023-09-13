5f106a6f2aac6c84b10b7f30e37ee91c3be6a944
leonardoneves <leonardo.neves@ilia.digital>
2023-09-13 16:35:24 -0300

    :hammer: add the option to provide an icon

--- gateapi/web/src/components/BaseTextInput.svelte
+++ gateapi/web/src/components/BaseTextInput.svelte
@@ -7,6 +7,9 @@
 	export let placeholder: string = ''
 	export let inputType: InputType = 'text'
 	export let readOnly: boolean = false
+	export let icon: ConstructorOfATypedSvelteComponent = null
+
+	$: icon ? (inputType = 'text') : (inputType = inputType)
 
 	const inputClasses = (): string => {
 		return `input-hover h-12 bg-white border-[1px] border-solid border-secondary 
@@ -15,13 +18,20 @@
 </script>
 
 <div
-	class="flex flex-1 flex-col md:max-w-md gap-2"
+	class="flex flex-1 flex-col relative md:max-w-md gap-2"
 	data-testid="base-text-input"
 >
 	<label for={id} class="text-primary text-s leading-4">
 		{label}
 	</label>
-	{#if inputType === 'number'}
+	{#if icon}
+		<div class="absolute">
+			<div class="absolute top-[34px] left-4">
+				<svelte:component this={icon} />
+			</div>
+		</div>
+	{/if}
+	{#if inputType === 'number' && !icon}
 		<input type="number" {id} bind:value {placeholder} class={inputClasses()} />
 	{:else}
 		<input
@@ -30,6 +40,7 @@
 			bind:value
 			{placeholder}
 			class={inputClasses()}
+			class:pl-12={icon}
 			disabled={readOnly}
 		/>
 	{/if}
