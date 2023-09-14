781e425b35c84cdf617fc17120ac180f02fe4dea
leonardoneves <leonardo.neves@ilia.digital>
2023-09-14 15:07:23 -0300

    :lipstick: create a base toast component

---
+++ gateapi/web/src/components/BaseToast.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition'
+
+	type ToastType = 'success' | 'error'
+
+	export let message: string
+	export let type: ToastType = 'success'
+</script>
+
+<div
+	transition:slide
+	class:bg-red={type === 'error'}
+	class="flex z-50 items-center justify-center shadow-primary-shadow fixed right-6 top-6 w-[240px] h-16 rounded-full bg-accent text-white"
+>
+	{message}
+</div>
